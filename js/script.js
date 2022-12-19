8a7dc52f94d77dec1632d9b27c8991417d048c10
Daniel Rodrigues <dnldvpr@gmail.com>
2022-12-19 13:55:41 -0300

    teste git

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDown from './modules/dropdown.js';
+import outsideClick from './modules/outsideclick.js';
+/* import setTimeOut from './modules/setTimeOut.js'; */
+
+
+
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropDown();
+outsideClick();
+setTimeOut();
